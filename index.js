5510dffa657a1affe5f20024a549c0c4f9f92dd0
Vidhi Ajmera <113877644+Vidhi-Ajmera16355@users.noreply.github.com>
2024-10-05 01:19:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-  origin: process.env.FRONTEND_URL || "http://localhost:5173", // Adjust as necessary
+  origin: "https://jp-frontend-theta.vercel.app/" || "http://localhost:5173", // Adjust as necessary
   credentials: true,
 };
 
